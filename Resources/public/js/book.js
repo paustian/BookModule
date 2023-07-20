d0874ef563cec8c77fd0cf050ddcdc57191d6b4d
Timothy Paustian <tdpaustian@gmail.com>
2023-07-20 13:06:11 -0500

    Highlight last read position on scroll
    Added a feature to highlight the last read position after a scroll.

--- Resources/public/js/book.js
+++ Resources/public/js/book.js
@@ -63,15 +63,46 @@ function getSelectionHtml2() {
     };
    /*
    This code is toying with the idea of highlighing where the reading spot when the text scrolls. It may be as simple as
-   having a div in an absolute position on the page and then flashing a highlight color to show the reading spot
+   having a div in an absolute position on the page and then flashing a highlight color to show the reading spot*/
    var iScrollPos = 0;
+   var iFirstVisible = 0;
+    function isInViewport(element) {
+        const rect = element.getBoundingClientRect();
+        var clientHeight = document.documentElement.clientHeight;
+        var windowWheight = window.innerHeight;
 
-    $(window).scroll(function () {
-        var iCurScrollPos = $(this).scrollTop();
-        if (iCurScrollPos > iScrollPos) {
-
+        if( rect.height <= 0 || rect.width <= 0){
+            return false;
         }
+        return (
+            rect.top >= window.screenTop &&
+            rect.left >= 0 &&
+            rect.bottom <= (window.innerHeight || document.documentElement.clientHeight) &&
+            rect.right <= (window.innerWidth || document.documentElement.clientWidth)
+        );
+    }
+    $(window).scroll(function () {
+        //This gets called with the scrolled positions, not before scroll.
+        var iCurScrollPos = $(this).scrollTop();
+        if(iFirstVisible){
+            iFirstVisible.removeClass("showposition");
+        }
+        //scroll down
+        //walk down until we see the first visible element.
+        if (iCurScrollPos > iScrollPos) {
+            var panel_elements = $("div.panel-body").children();
+            panel_elements.each(function(i){
+                if(isInViewport(this)){
+                    iFirstVisible = $(this);
+                    return false;
+                }
+            });
+            //Now hightlight it for a second and then fade away.
+            iFirstVisible.addClass("showposition");
+            setTimeout(() => {iFirstVisible.removeClass("showposition");}, 1000);
+        }
+
         iScrollPos = iCurScrollPos;
-    });*/
+    });
 })(jQuery);
 
